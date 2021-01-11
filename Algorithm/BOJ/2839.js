7486cf6fd63d7502821267457fe2ccd16e767fbf
yurim <pul8219@naver.com>
2021-01-12 00:31:55 +0900

    210112 algorithm TIL

--- Algorithm/BOJ/2839.js
+++ Algorithm/BOJ/2839.js
@@ -1,54 +1,26 @@
 // BOJ
 // 2389번 문제: 설탕 배달
 
+var n = Number(require('fs').readFileSync('/dev/stdin')); // BOJ 제출용 코드. 입력값을 받는 코드이며 입력값을 숫자로 받기 위해 Number로 감쌈 // 초기 n은 봉지에 나눠담을 설탕 n킬로그램을 의미 
 
-
-// 최대한 적은 봉지를 들고 가는 것이 목표
-// 5킬로그램의 봉지가 많아야 봉지수가 적어짐(당연한 것)
-
-// math.floor 써서 바꿔보기
-//이거 제출 통과아아악
-var n = Number(require('fs').readFileSync('/dev/stdin')); // BOJ 제출용 코드. 입력값을 받는 코드이며 입력값을 숫자로 받기 위해
-
-let three = 0;
-let five = Math.floor(n / 5);
+let three = 0; // 3kg짜리 봉지 개수
+let five = Math.floor(n / 5); // 5kg짜리 봉지 개수
 n %= 5;
 
 while(five >= 0){
-    if(n % 3 == 0){
-        three = Math.floor(n / 3);
-        n %= 3;
+    if(n % 3 == 0){ // 5kg 봉지에 담고 남은 나머지 설탕의 무게가 3kg 봉지에 딱 나눠떨어지게 담긴다면
+        three = Math.floor(n / 3); // 3kg짜리 봉지 개수 갱신
+        n %= 3; // n은 0일 것 // 봉지에 담아야할 설탕을 모두 담았음
         break;
 
     }
-    five--;
-    n += 5;
+    five--; // 5kg 봉지에 담고 남은 나머지 설탕의 무게가 3kg 봉지에 딱 떨어지게 담길 수 없다면 five를 감소시킴(5kg 짜리 봉지수를 한 개 감소시키는 것)
+    n += 5; // 5kg 봉지를 감소시켰으니 남은 설탕의 무게도 +5 해줌
 }
 
-if (n==0){
-    console.log(five + three);
+if (n==0){ // 봉지에 모두 잘 담겼다면
+    console.log(five + three); // 봉지개수 출력
 }
-else{
-    console.log(-1);
+else{ // 정확하게 N킬로그램을 만들 수 없다면
+    console.log(-1); // -1 출력
 }
-
-//이건 통과 안됐음
-var n = Number(require('fs').readFileSync('/dev/stdin'));
-//let n = 18;
-const big = 5;
-const small = 3;
-
-let bigMax = Math.floor(n / big);
-while(bigMax>=0){
-    let remain = n - bigMax * big;
-    if(remain % small === 0){
-        console.log(bigMax + n / small);
-        return;
-    }
-    else{
-        bigMax--;
-    }
-
-
-}
-console.log(-1);
